2e1c91f437a3594ca0218ba9cc64c24897e0df5f
diego Au pinzon <diegoaupinzonca@hotmail.com>
2026-04-02 12:10:27 -0500

    update local linux npm install

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 const App = () => {
@@ -14,7 +11,7 @@ const App = () => {
     <div>
       <p>Hello world    {nowTIME.toString()}</p>
       <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum laboriosam iusto quis suscipit! Optio illum soluta accusantium vitae delectus, illo cumque commodi voluptatum, unde, corporis deserunt voluptas odit atque.</div>
-    
+
       <div></div>
     </div>
   )
