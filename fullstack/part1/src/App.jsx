c594ccd8a8e3c482c95a438c84bca37b95d5b7b8
diego Au pinzon <diegoaupinzonca@hotmail.com>
2026-04-03 19:32:51 -0500

    init part1

---
+++ fullstack/part1/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  const course = "Half Stack application development  with React and Node.js"
+const Header = () =>{
+  return(
+    <h1>{course}</h1>
+  )
+
+}
+
+
+  return (
+    <Header />
+
+  )
+
+}
+export default App
